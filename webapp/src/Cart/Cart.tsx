8c51bc0d4331d7be407a7a9572315bccf351408f
Mario <UO275874@uniovi.es>
2022-03-03 19:17:18 +0100

    Comienzo Fron-end, carrito funcional
    He creado un carrito en base a un curso para aprender sobre React.

---
+++ webapp/src/Cart/Cart.tsx
@@ -0,0 +1,38 @@
+import CartItem from '../CartItem/CartItem';
+
+//Styles
+import {Wrapper} from './Cart.styles';
+
+//Types
+import { CartItemType } from '../App';
+
+
+
+type Props = {
+    cartItems: CartItemType[];
+    addToCart: (clickedItem:CartItemType)=> void;
+    removeFromCart: (id:number) => void;
+};
+
+const Cart:React.FC<Props> = ({cartItems, addToCart, removeFromCart})=> {
+    const calculateTotal = (items:CartItemType[]) =>
+     items.reduce((ack:number, item) => ack + item.amount*item.price,0);
+
+    return (
+        <Wrapper>
+            <h2>Tu Carrito</h2>
+            {cartItems.length===0 ? <p>No hay juguetes en el carrito</p>: null}
+            {cartItems.map(item=>(
+                <CartItem 
+                    key={item.id}
+                    item={item}
+                    addToCart={addToCart}
+                    removeFromCart={removeFromCart}
+                />
+            ))}
+            <h2>Total: €{calculateTotal(cartItems).toFixed(2)}</h2>
+        </Wrapper>
+    )
+};
+
+export default Cart;
