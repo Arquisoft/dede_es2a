f1ed3f022713d5c220c8ddb879aa6a709f4c29aa
Alejandro Galán Freire <UO277346@uniovi.es>
2022-03-07 20:03:07 +0100

    Modelo juguete
    Reajuste en el modelo de los juguetes, configuración de sus atributos

--- restapi/models/Juguete.ts
+++ restapi/models/Juguete.ts
@@ -1,5 +1,5 @@
 import {ObjectId} from "mongodb";
 
 export default class Juguete {
-    constructor(public name: string, public description: string, public id?: ObjectId){}
+    constructor(public name: string, public description: string, public precio: number, public imagen: string, public categoria: string ,public id?: ObjectId){}
 }
