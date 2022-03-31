f5d9660a005230a7582498944fd021fb19ead7f4
uo277819 <uo277819@uniovi.es>
2022-03-31 21:37:23 +0200

    Test enrutamiento

---
+++ webapp/src/tests/dynamicrouting.test.tsx
@@ -0,0 +1,74 @@
+import '@testing-library/jest-dom/extend-expect';
+import {fireEvent, render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import LoginButton from '../componentes/Login/LoginButton';
+import LogoutButton from '../componentes/Login/LogoutButton'
+import Navbar from '../componentes/Navbar/Navbar';
+import Footer from '../componentes/Footer/Footer';
+import Home from '../paginas/Home';
+import ContactUs from '../paginas/ContactUs';
+import App from '../App';
+
+test('navbar is rendered', () => {
+  const component = render(<Navbar/>)
+  expect(component.container).toHaveTextContent('DeNostalgia')
+})
+
+test('home is rendered', () => {
+  const component = render(<Home/>)
+  expect(component.container).toHaveTextContent('Bienvenido')
+})
+
+test('ContactUs is rendered', () => {
+  const component = render(<ContactUs/>)
+  expect(component.container).toHaveTextContent('Contáctanos')
+})
+
+test('footer is rendered', () => {
+  const component = render(<Footer/>)
+
+  expect(component.container).toHaveTextContent('Uniovi')
+})
+
+test('clicking home nav-button', () => {
+  const component = render(<Navbar/>)
+
+  const mockHandler = jest.fn()
+  //const component2=render(<App/>).container
+  const button = component.getByText('Home')
+  fireEvent.click(button)
+  expect(mockHandler).toHaveBeenCalledTimes(1)
+  //expect(component.container).toHaveTextContent('Bienvenido')
+})
+
+//Falta hacer que funcione render(<App>)
+
+test('clicking products nav-button', () => {
+  const component = render(<Navbar/>)
+
+  const mockHandler = jest.fn()
+  const button = component.getByText('Productos')
+  fireEvent.click(button)
+  expect(mockHandler).toHaveBeenCalledTimes(1)
+})
+
+test('clicking contact nav-button', () => {
+  const component = render(<Navbar/>)
+
+  const mockHandler = jest.fn()
+  const button = component.getByText('Contáctanos')
+  fireEvent.click(button)
+  expect(mockHandler).toHaveBeenCalledTimes(1)
+})
+
+test('clicking login button', () => {
+  const component = render(<LoginButton/>)
+
+  const mockHandler = jest.fn()
+
+  const button = component.getByText('Registrarse')
+  fireEvent.click(button)
+  
+  expect(mockHandler).toHaveBeenCalledTimes(1)
+})
+
