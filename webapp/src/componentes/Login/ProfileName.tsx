e2a6edd5c3e231653f354c87d131b468f305f6fe
Jorge <uo276514@uniovi.es>
2022-04-01 18:57:56 +0200

    Información del perfil en navbar
    Si autenticado

---
+++ webapp/src/componentes/Login/ProfileName.tsx
@@ -0,0 +1,13 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import 'react-json-pretty/themes/monikai.css';
+
+const ProfileName = () => {
+    const { user } = useAuth0();
+    return (
+        <div className='label prof-name'>
+            <p>{user?.name}</p>
+        </div>
+    );
+}
+
+export default ProfileName;
