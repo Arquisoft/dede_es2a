837cd2b0d792fd80c8f8b369cacfbc974778b2c5
Mario <UO275874@uniovi.es>
2022-04-02 12:58:46 +0200

    Ventana de pago

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -17,6 +17,7 @@ import Badge from '@material-ui/core/Badge';
 
 //Styles
 import { Wrapper, StyledButton } from './App.styles';
+import PayForm from './PayForm/PayForm';
 
 
 const inlineStyles= {
@@ -61,8 +62,8 @@ export async function getJuguetes():Promise<Juguete[]>{
 en la BD y exportando para poder usarlo desde fuera*/
 //--------------------------------------------------------------------------------------------------------
 
-const getProducts = async (): Promise<CartItemType[]> =>
-  await (await fetch('https://fakestoreapi.com/products')).json();
+
+
 
 const App = () => {
 
@@ -123,38 +124,7 @@ const App = () => {
 
   };
 
-  /*const handleAddToCart = (clickedItem: CartItemType) => {
-    //"prev" es el estado previo del carrito, justo antes de añadir un producto
-    setCartItems(prev => {
-      //1. Teniamos ya el producto en el carrito
-      const isItemInCart = prev.find(item => item.id ===clickedItem.id)
-      if(isItemInCart) {
-        return prev.map(item=>(
-          item.id===clickedItem.id
-          //Cogemos el objeto viejo y le aumentamos la amount. Si no tenemos el item en el carrito, el item viejo se devuelve tal y como estaba(pòrque no es el mismo)
-            ? {...item, amount: item.amount+1}
-            : item
-        ))
-      }
-      //2. El producto no está en el carrito, tenemos que añadirlo como uno nuevo
-      //Entonces lo que hacemos es retornar el estado previo (prev) y le añadimos una nueva casilla que tienen el clickedItem con un amount de 1
-      return [...prev, {...clickedItem, amount:1}];
-    })
-  };*/
 
-  /*const handleRemoveFromCart = (id: number) => {
-    setCartItems(prev=>(
-      prev.reduce((ack, item)=> {
-        if(item.id===id){
-          if(item.amount===1) return ack;
-          return [...ack, {...item, amount:item.amount - 1}]
-        } else {
-          return [...ack, item];
-        }
-      }, [] as CartItemType[])
-    ))
-
-  };*/
 
   //Coloca una barra de carga cuando la página está cargando
   if (isLoading) return <LinearProgess />;
@@ -173,6 +143,7 @@ const App = () => {
             addToCart={handleAddToCart}
             removeFromCart={handleRemoveFromCart}
           />
+        
         </Drawer>
         <StyledButton onClick={() => setCartOpen(true)}>
           <Badge badgeContent={getTotalItems(cartItems)} color='error'>
