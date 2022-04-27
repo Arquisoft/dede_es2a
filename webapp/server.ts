84343b34af4fadb37a01d1c1f55a4b03f09f698a
Mario <UO275874@uniovi.es>
2022-04-27 18:25:03 +0200

    Intento heroku

--- webapp/server.ts
+++ webapp/server.ts
@@ -1,9 +1,26 @@
 import express,{Application} from 'express'; 
 //for using an import here we need to configure the tsconfig.json
 //setting the option module to commonjs
+const mongoose = require('mongoose');
+
 
 var app: Application = express()
-const port: number = 3000;
+const port = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:dede2a@tiendajuguetes.1s9n2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected',()=> {
+    console.log('Mongoose is connected!')
+})
+
+if(process.env.NODE_ENV==='production') {
+    
+}
+
+
 
 app.use(express.static('build'))
 
