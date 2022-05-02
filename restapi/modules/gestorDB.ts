844994d4713affa43c54ba99082bc08333af9f06
Rafael Muñiz <uo271728@uniovi.es>
2022-05-02 11:56:26 +0200

    Arreglo cors y variables de entorno

--- restapi/modules/gestorDB.ts
+++ restapi/modules/gestorDB.ts
@@ -2,11 +2,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const database_uri = process.env.DB_CONNECT
-const database_test_uri = 'mongodb+srv://admin:<password>@cluster.mf7ve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
 export function connect(){
-    mongoose.connect('mongodb+srv://admin:dede2a@tiendajuguetes.1s9n2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    mongoose.connect(process.env.MONGO_URI, {
         
     })
     .then(() =>{
--- restapi/server.ts
+++ restapi/server.ts
@@ -18,7 +18,7 @@ let {config} = require('./modules/cloudinary');
 
 const options: cors.CorsOptions = {
   //origin: ['http://localhost:3000']
-  origin: ['https://dede-es2a-webapp.herokuapp.com']
+  origin: ['https://dede-es2a-webapp.herokuapp.com' || 'http://localhost:3000']
 };
 
 const metricsMiddleware:RequestHandler = promBundle({includeMethod: true});
